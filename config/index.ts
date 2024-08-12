720b26855b57ec663c7f178d58f77d89712172b4
sylvor <william.moore.9115@gmail.com>
2024-08-12 15:16:36 -0700

    add config.js

---
+++ config/index.ts
@@ -0,0 +1,26 @@
+/**
+ * default config
+ */
+export const config = {
+  title: [
+    "True friends are never apart, maybe in distance but never in heart.",
+    "The stars hang low over the vast plain, the moon surges with the great river.",
+    "A single plume of smoke rises straight in the desert, the long river sets round against the sun.",
+    "Why UI designers should understand Flexbox and CSS Grid",
+  ],
+  subTitle: "",
+  author: "@ConvImg",
+  font: "font-anke",
+  icon: "",
+  backColor: "#1F2937",
+  backBlurLevel: 60,
+  deviconValue: ["aarch64-plain"],
+  aspect: "aspect-[16/9]",
+  blur: 0,
+  blurTrans: 60,
+  logoPosition: "default",
+  fontSize: 40,
+  authorFontSize: 25,
+};
+
+export type Config = typeof config;
