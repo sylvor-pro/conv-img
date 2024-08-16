66d38cacf2bc1f2f7366701e27cb9cd6899a8d05
sylvor <william.moore.9115@gmail.com>
2024-08-16 09:39:20 -0700

    icons-components

---
+++ app/[locale]/icons/BottomLeftIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+export const BottomLeftIcon = (props) => (
+  <svg
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="0.125em"
+    className="css-z61snw"
+    width="24"
+    height="24"
+    fill="none"
+    {...props}
+  >
+  <path
+    d="M10 21H21V3L3 3L3 14"
+    stroke="currentcolor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  ></path>
+  <circle cx="4.5" cy="19.5" r="2.5" stroke="currentcolor"></circle>
+  </svg>
+);
+ 
